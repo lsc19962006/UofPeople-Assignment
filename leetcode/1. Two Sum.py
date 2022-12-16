a270986ec3084a426ff554b2d0b40bd2088e0bad
lsc19962006 <111042169+lsc19962006@users.noreply.github.com>
2022-12-16 14:38:15 +1100

    n

--- leetcode/1. Two Sum.py
+++ leetcode/1. Two Sum.py
@@ -1,8 +1,22 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            p=target-nums[i]
-            num=nums
-            num[i]=999999999
-            if p in num:
-                return [i,num.index(p)]
+# define the function to draw a heart
+def draw_heart():
+  # draw the top half of the heart
+  for y in range(12, -12, -1):
+    for x in range(-15, 16):
+      if (x*x + y*y - 225)**3 - x*x*y*y*y <= 0:
+        print("*", end="")
+      else:
+        print(" ", end="")
+    print()
+
+  # draw the bottom half of the heart
+  for y in range(-11, 12):
+    for x in range(-15, 16):
+      if (x*x + y*y - 225)**3 - x*x*y*y*y <= 0:
+        print("*", end="")
+      else:
+        print(" ", end="")
+    print()
+
+# call the function to draw the heart
+draw_heart()
